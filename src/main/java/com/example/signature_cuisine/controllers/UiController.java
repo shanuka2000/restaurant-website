73de96a9708825999889d563867f15c1e7f21f2d
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-06 18:28:39 +0530

    Added Controller class for load UI

---
+++ src/main/java/com/example/signature_cuisine/controllers/UiController.java
@@ -0,0 +1,10 @@
+package com.example.signature_cuisine.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UiController {
+    @GetMapping("/")
+    public String getHome() {return  "home";}
+}
