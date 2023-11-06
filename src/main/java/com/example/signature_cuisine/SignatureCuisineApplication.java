8203950258b722d3c4200bde93481b0c3683d98b
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-06 18:28:09 +0530

    Initial commit

---
+++ src/main/java/com/example/signature_cuisine/SignatureCuisineApplication.java
@@ -0,0 +1,13 @@
+package com.example.signature_cuisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatureCuisineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignatureCuisineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/signature_cuisine/SignatureCuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signature_cuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignatureCuisineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
