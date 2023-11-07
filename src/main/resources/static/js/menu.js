00e7271663630f73af42cb7ec3997e80bc6fc597
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-08 00:17:12 +0530

    Fixed menu structure issue and completed menu display

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,67 +1,52 @@
 $(document).ready(function () {
     $.getJSON('/json/menu.json', function (data) {
         const cuisinesArr = data.cuisines;
-
-        // Flags to track if cuisine and category have been displayed
-        let cuisineDisplayed = false;
-        let categoryDisplayed = false;
-
         $.each(cuisinesArr, function (index, item) {
             const cuisineName = item.name;
             const cuisineCategoriesArr = item.categories;
 
-            // Display cuisine only once
-            if (!cuisineDisplayed) {
-                const cuisineHtml =
-                    '<ul class="category">' +
-                        '<li class="category-heading">' +
-                            '<button class="controlBtn">' +
-                                '<ion-icon name="chevron-expand-outline"></ion-icon>' +
-                                '<span>Our ' + cuisineName + ' Menu</span>' +
-                            '</button>' +
-                        '</li>' +
+            let cuisineHtml =
+                '<ul class="category">' +
+                    '<li class="category-heading">' +
+                        '<button class="controlBtn">' +
+                            '<ion-icon name="chevron-expand-outline"></ion-icon>' +
+                            '<span>Our ' + cuisineName + ' Menu</span>' +
+                        '</button>' +
+                    '</li>' +
                     '<ul class="category-sec">';
-                $('.section-menu').append(cuisineHtml);
-                cuisineDisplayed = true;
-            }
 
             $.each(cuisineCategoriesArr, function (index, subItem) {
                 const subCategoryName = subItem.name;
                 const foodList = subItem.items;
 
-                // Display category only once
-                if (!categoryDisplayed) {
-                    const categoryHtml =
-                        '<li class="subCategory-heading">' +
-                            '<button class="controlBtn">' +
-                                '<ion-icon name="chevron-expand-outline"></ion-icon>' +
-                                '<span>' + subCategoryName + '</span>' +
-                            '</button>' +
-                        '</li>' +
-                        '<ul class="category-item">';
-                    $('.category-sec').append(categoryHtml);
-                    categoryDisplayed = true;
-                }
+                let categoryHtml =
+                    '<li class="subCategory-heading">' +
+                        '<button class="controlBtn">' +
+                            '<ion-icon name="chevron-expand-outline"></ion-icon>' +
+                            '<span>' + subCategoryName + '</span>' +
+                        '</button>' +
+                    '</li>' +
+                    '<ul class="category-item">';
 
                 $.each(foodList, function (index, foodItem) {
                     const foodName = foodItem.name;
                     const foodPrice = foodItem.price;
 
-                    const htmlString =
+                    let foodItemHtml =
                         '<li>' +
                             '<p>' + foodName + '</p>' +
                             '<hr class="hr"/>' +
-                            '<p>' + foodPrice + '</p>' +
-                        '</li>' +
-                        '</ul></ul>'
-                    $('.category-item').append(htmlString);
+                            '<p>' + foodPrice +'</p>' +
+                        '</li>';
+
+                    categoryHtml += foodItemHtml
                 })
+                categoryHtml += '</ul>'
+                cuisineHtml += categoryHtml;
             })
+
+            cuisineHtml += '</ul></ul>'
+            $('.section-menu').append(cuisineHtml);
         })
     })
 })
-
-
-
-
-
