8f792f8dcbc249b9d7f8308d7304620f6f1bceb4
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-07 23:34:41 +0530

    Added cors policy

---
+++ src/main/java/com/example/signature_cuisine/configuration/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.signature_cuisine.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*"); // You can specify specific origins here
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
