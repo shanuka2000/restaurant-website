09090aa4a924336e01699d43624ac5d183af294a
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-09 05:17:50 +0530

    Added database configs

--- src/main/java/com/example/signature_cuisine/util/PasswordHasher.java
+++
@@ -1,6 +0,0 @@
-package com.example.signature_cuisine.util;
-
-public interface PasswordHasher {
-    String hashPassword(String password);
-    boolean isPasswordValid(String password, String hashedPassword);
-}
