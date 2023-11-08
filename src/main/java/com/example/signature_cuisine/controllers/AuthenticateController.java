20ca2c4d2b1a96c3bbe51ac09b3957900d435e9b
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-09 05:18:23 +0530

    Completed authentication controller for admin, staff and customer auth

---
+++ src/main/java/com/example/signature_cuisine/controllers/AuthenticateController.java
@@ -0,0 +1,69 @@
+package com.example.signature_cuisine.controllers;
+
+import com.example.signature_cuisine.exception.ErrorResponse;
+import com.example.signature_cuisine.services.impl.authentication.AuthenticateServiceAdminImpl;
+import com.example.signature_cuisine.services.impl.authentication.AuthenticateServiceCustomerImpl;
+import com.example.signature_cuisine.services.impl.authentication.AuthenticationStaffImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticateController {
+
+    @Autowired
+    private AuthenticateServiceCustomerImpl authenticateServiceCustomer;
+
+    @Autowired
+    private AuthenticationStaffImpl authenticateStaff;
+
+    @Autowired
+    private AuthenticateServiceAdminImpl authenticateServiceAdmin;
+
+    @RequestMapping(value = "/customer", method = RequestMethod.POST)
+    public ResponseEntity<?> authenticateCustomer(@RequestParam("email") String email, @RequestParam("password") String password) {
+
+        try {
+            boolean authenticated = authenticateServiceCustomer.authenticate(email, password);
+
+            return getStringResponseEntity(authenticated);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    @RequestMapping(value = "/staff", method = RequestMethod.POST)
+    public ResponseEntity<?> authenticateStaff(@RequestParam("email") String email, @RequestParam("password") String password) {
+
+        try {
+            boolean authenticated = authenticateStaff.authenticate(email, password);
+
+            return getStringResponseEntity(authenticated);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.POST)
+    public ResponseEntity<?> authenticateAdmin(@RequestParam("email") String email, @RequestParam("password") String password) {
+
+        try {
+            boolean authenticated = authenticateServiceAdmin.authenticate(email, password);
+
+            return getStringResponseEntity(authenticated);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    private static ResponseEntity<String> getStringResponseEntity(boolean authenticated) {
+        if (authenticated) {
+            return ResponseEntity.status(HttpStatus.OK).body("Login Successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+    }
+}
