4cfb843c8b61e0c9c4e2fe4d1bfe0bfb685b4712
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-09 23:48:55 +0530

    Availability check by guest count and reservation type

---
+++ src/main/java/com/example/signature_cuisine/services/impl/reservation/CheckAvailabilityByGuestCount.java
@@ -0,0 +1,20 @@
+package com.example.signature_cuisine.services.impl.reservation;
+
+import com.example.signature_cuisine.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class CheckAvailabilityByGuestCount {
+    private final int MAX_GUESTS = 50;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    public boolean isAvailable(Date reservationDate, String reservationType, int reservationGuestCount) {
+        Integer guestCount = reservationRepository.getTotalGuestsByDateAndType(reservationDate, reservationType);
+        return guestCount == null || guestCount < MAX_GUESTS || guestCount + reservationGuestCount < MAX_GUESTS;
+    }
+}
