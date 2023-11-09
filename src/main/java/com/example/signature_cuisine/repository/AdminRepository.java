f955a25515c6bb5628b25f4961a1562fc4718674
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-09 05:37:08 +0530

    Added Admin related service and repository classes

--- src/main/java/com/example/signature_cuisine/repository/AdminRepository.java
+++ src/main/java/com/example/signature_cuisine/repository/AdminRepository.java
@@ -4,4 +4,5 @@ import com.example.signature_cuisine.entity.AdminEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<AdminEntity, Long> {
+    AdminEntity findByEmail(String email);
 }
---
+++ src/main/java/com/example/signature_cuisine/services/impl/authentication/AuthenticateServiceAdminImpl.java
@@ -0,0 +1,31 @@
+package com.example.signature_cuisine.services.impl.authentication;
+
+import com.example.signature_cuisine.entity.AdminEntity;
+import com.example.signature_cuisine.repository.AdminRepository;
+import com.example.signature_cuisine.services.AuthenticateService;
+import com.example.signature_cuisine.util.PasswordHash;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticateServiceAdminImpl implements AuthenticateService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    private PasswordHash passwordHash;
+
+    public AuthenticateServiceAdminImpl(PasswordHash passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    @Override
+    public boolean authenticate(String email, String password) throws Exception {
+        try {
+            AdminEntity adminEntity = adminRepository.findByEmail(email);
+            return passwordHash.isPasswordValid(password, adminEntity.getPassword());
+        } catch (Exception e) {
+            throw new Exception("Something went wrong!");
+        }
+    }
+}
