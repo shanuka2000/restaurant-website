a2558ce63dfbfb07d7b123eed313b3f66814f664
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-10 02:02:04 +0530

    Implemented quick availability check

--- src/main/resources/static/js/reservation.js
+++ src/main/resources/static/js/reservation.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $("#availabilityStatus").hide();
     $(".customInput").on('input', function () {
         const inputValue = $(this).val();
         const outputId = $(this).data('output-id');
@@ -8,13 +9,34 @@ $(document).ready(function () {
     })
 })
 
-$("#check-avb").submit(function (event) {
+$("#summary-form").submit(function (event) {
     event.preventDefault();
 
     const reservationDate = $("#iReservationDate").val();
     const numberOfGuests = $("#iNumPeople").val();
+    const reservationType = $("#iReservationType").val();
+    const availabilityIndicator = $("#availabilityStatus");
 
-    // TODO: Implement code to check availability
+    availabilityIndicator.hide();
+
+    $.get("http://localhost:8080/api/reservation/availability?reservationDate="+ reservationDate +"&reservationType="+ reservationType +"&guestCount="+numberOfGuests)
+        .done(function (data) {
+            availabilityIndicator.show();
+            availabilityIndicator.removeClass().addClass("availability-success");
+            availabilityIndicator.text("Available")
+        })
+        .fail(function (error) {
+
+            if (error.status === 409) {
+                availabilityIndicator.show();
+                availabilityIndicator.removeClass().addClass("availability-failed");
+                availabilityIndicator.text("Not Available")
+            } else {
+                availabilityIndicator.show();
+                availabilityIndicator.removeClass().addClass("availability-failed");
+                availabilityIndicator.text("Not Enough Data")
+            }
+        })
 })
 
 $("#reservation-form").submit(function (event) {
