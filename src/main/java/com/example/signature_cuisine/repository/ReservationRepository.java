07394affac809191ead1893e57f9d8a4c5fcbc45
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-09 23:52:15 +0530

    Added query to getTotalGuestsByDateAndType and find all reservations by date

--- src/main/java/com/example/signature_cuisine/repository/ReservationRepository.java
+++ src/main/java/com/example/signature_cuisine/repository/ReservationRepository.java
@@ -2,10 +2,17 @@ package com.example.signature_cuisine.repository;
 
 import com.example.signature_cuisine.entity.ReservationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
     ReservationEntity save(ReservationEntity reservationEntity);
     List<ReservationEntity> findAll();
+    List<ReservationEntity> findAllByReservationDate(Date date);
+
+    @Query("SELECT SUM(r.numberOfGuest) FROM ReservationEntity r WHERE r.reservationDate = :reservationDate AND r.reservationType = :reservationType")
+    Integer getTotalGuestsByDateAndType(@Param("reservationDate") Date reservationDate, @Param("reservationType") String reservationType);
 }
