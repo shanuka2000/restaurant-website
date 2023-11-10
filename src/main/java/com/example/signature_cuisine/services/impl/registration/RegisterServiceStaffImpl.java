4382ca780e864ea20fa9bad0308e76a386603534
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-10 22:14:24 +0530

    Implemented staff register service

---
+++ src/main/java/com/example/signature_cuisine/services/impl/registration/RegisterServiceStaffImpl.java
@@ -0,0 +1,39 @@
+package com.example.signature_cuisine.services.impl.registration;
+
+import com.example.signature_cuisine.entity.CustomerEntity;
+import com.example.signature_cuisine.entity.StaffEntity;
+import com.example.signature_cuisine.repository.StaffRepository;
+import com.example.signature_cuisine.services.RegisterService;
+import com.example.signature_cuisine.services.impl.security.Hash256;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterServiceStaffImpl implements RegisterService  {
+    @Autowired
+    private StaffRepository staffRepository;
+    private Hash256 passwordHash;
+    private ValidateEmail validateEmail;
+    public RegisterServiceStaffImpl(Hash256 passwordHash, ValidateEmail validateEmail) {
+        this.passwordHash = passwordHash;
+        this.validateEmail = validateEmail;
+    }
+    @Override
+    public boolean register(String fullName, String email, String password) throws Exception {
+        try {
+            StaffEntity staffEntity = new StaffEntity();
+
+            staffEntity.setFullName(fullName);
+            staffEntity.setEmail(email);
+            staffEntity.setPassword(passwordHash.hashPassword(password));
+
+            if (validateEmail.isValid(email)) {
+                return staffRepository.save(staffEntity) != null;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            throw new Exception("Internal server error occurred. Please contact the administrator.");
+        }
+    }
+}
