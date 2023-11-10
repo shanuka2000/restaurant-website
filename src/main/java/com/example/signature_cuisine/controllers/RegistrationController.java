336569a8e8de53f5666ca3224fed95d0e064983d
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-10 22:22:15 +0530

    Added staff registration Post method

--- src/main/java/com/example/signature_cuisine/controllers/RegistrationController.java
+++ src/main/java/com/example/signature_cuisine/controllers/RegistrationController.java
@@ -2,7 +2,9 @@ package com.example.signature_cuisine.controllers;
 
 import com.example.signature_cuisine.exception.ErrorResponse;
 import com.example.signature_cuisine.model.Customer;
+import com.example.signature_cuisine.model.Staff;
 import com.example.signature_cuisine.services.impl.registration.RegisterServiceCustomerImpl;
+import com.example.signature_cuisine.services.impl.registration.RegisterServiceStaffImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,18 +17,36 @@ public class RegistrationController {
     @Autowired
     private RegisterServiceCustomerImpl registerServiceCustomer;
 
+    @Autowired
+    private RegisterServiceStaffImpl registerServiceStaff;
+
     @PostMapping("/customer")
     public ResponseEntity<?> registerCustomer(@RequestBody Customer customer) {
         try {
             boolean isRegistered = registerServiceCustomer.register(customer.getFullName(), customer.getEmail(), customer.getPassword());
 
-            if (isRegistered) {
-                return ResponseEntity.status(HttpStatus.CREATED).body("Registration Successful");
-            } else {
-                return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Registration Failed");
-            }
+            return getStringResponseEntity(isRegistered, "Customer");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    @PostMapping("/staff")
+    public ResponseEntity<?> registerStaff(@RequestBody Staff staff) {
+        try {
+            boolean isRegistered = registerServiceStaff.register(staff.getFullName(), staff.getEmail(), staff.getPassword());
+
+            return getStringResponseEntity(isRegistered, "Staff");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    private static ResponseEntity<String> getStringResponseEntity(boolean isRegistered, String userType) {
+        if (isRegistered) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(userType + " registration successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(userType + " registration failed");
         }
     }
 }
