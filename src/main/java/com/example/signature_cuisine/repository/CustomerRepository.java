825cb9b600ebf44e01fd61d6dc1d4c31609fbc91
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-10 18:36:05 +0530

    Added customer save method for CustomerRepository

--- src/main/java/com/example/signature_cuisine/repository/CustomerRepository.java
+++ src/main/java/com/example/signature_cuisine/repository/CustomerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
     CustomerEntity findByEmail(String email);
+    CustomerEntity save(CustomerEntity customer);
 }
